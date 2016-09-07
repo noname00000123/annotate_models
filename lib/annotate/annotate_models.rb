a033386f768a6c6247aaba7603b38240bc8dab64
noname00000123 <noname00000123@users.noreply.github.com>
2016-09-08 00:07:59 +1000

    Update annotate_models.rb

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -385,12 +385,14 @@ module AnnotateModels
 
         association = "# - #{macro}{##{name}}"
         
-        next if assoc.options.key?(:polymorphic)
+        # next if assoc.options.key?(:polymorphic)
         
         association += if assoc.is_a?(ActiveRecord::Reflection::ThroughReflection)
           " {#{assoc.delegate_reflection.klass.name}} *through* {##{assoc.options[:through]}}\n"
         elsif Object.const_defined?(assoc.class_name)
           " {#{assoc.class_name}}\n"
+        elsif assoc.options.key?(:polymorphic)
+          "\n"
         else
           " {#{assoc.class_name}}\n"
         end
